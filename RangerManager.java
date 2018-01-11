4db43d998cfa905877c815b82cf71e8234f52067
KerbalsRock <bbishop19@windwardschool.org>
2018-01-10 20:32:59 -0800

    add rangermanager back to normal

--- RangerManager.java
+++ RangerManager.java
@@ -1,4 +1,11 @@
+import bc.GameController;
+	public class RangerManager extends AttackUnitManager{
 
-public class RangerManager {
-//if true this will show up in Github
+		public RangerManager(GameController gc) {
+			super(gc);
+		}
+		public RangerManager(GameController gc, int id ) {
+			super(gc, id);
+		}
+
 }
