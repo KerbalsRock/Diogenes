70ee7eed909fc2744ea69140adb63c5de1dcab2a
KerbalsRock <bbishop19@windwardschool.org>
2018-01-10 20:35:56 -0800

    add lucasvision

--- WorkerManager.java
+++ WorkerManager.java
@@ -1,4 +1,10 @@
 import bc.*;
-public class WorkerManager {
-	
+public class WorkerManager extends UnitManager{
+		public WorkerManager(GameController gc) {
+			super(gc);
+		}
+		public WorkerManager(GameController gc, int id ) {
+			super(gc, id);
+		}
+
 }
