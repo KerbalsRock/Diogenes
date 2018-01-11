e3fc9e9f40246cc211fbab4bd1bec2ce6fa951eb
KerbalsRock <bbishop19@windwardschool.org>
2018-01-10 20:14:48 -0800

    Add all moveable unit classes

---
+++ HealerManager.java
@@ -0,0 +1,11 @@
+import bc.GameController;
+public class HealerManager extends AttackUnitManager{
+
+	public HealerManager(GameController gc) {
+		super(gc);
+	}
+	public HealerManager(GameController gc, int id ) {
+		super(gc, id);
+	}
+
+}
