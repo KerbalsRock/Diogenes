9d3873a4d9bcc0691eba600064f2d5bf91bd4db7
KerbalsRock <bbishop19@windwardschool.org>
2018-01-11 11:14:19 -0800

    Add path + pathmaker

---
+++ Path.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import bc.*;
+public class Path {
+	public ArrayList<MapLocation> locList;
+	
+	public Path() {}
+	
+	public Path(ArrayList<MapLocation> list) {
+		locList = list;
+	}
+	
+}
---
+++ PathMaker.java
@@ -0,0 +1,13 @@
+import bc.*;
+public class PathMaker {
+	PlanetMap p;
+	
+	public PathMaker(PlanetMap p){
+		this.p = p;
+	}
+	
+	public Path generatePath(MapLocation start, MapLocation end) {
+		//TODO make this work
+		return null;
+	}
+}
