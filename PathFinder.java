2bfb4da54478312854c8061a4446bfe342471aef
KerbalsRock <bbishop19@windwardschool.org>
2018-01-14 20:34:19 -0800

    IT WORKS

--- PathFinder.java
+++ PathFinder.java
@@ -24,8 +24,6 @@ public class PathFinder {
 	}
 
 	public Path generatePath(MapLocation startLoc, MapLocation endLoc) {
-		/*AStarNode tempNode =search(island.convertToHashMap().get(startLoc.toString()),
-				island.convertToHashMap().get(endLoc.toString()));*/
 		AStarNode startNode = island.convertToHashMap().get(startLoc.toString());
 		AStarNode endNode = island.convertToHashMap().get(endLoc.toString());
 		PriorityQueue<AStarNode> openSet = new PriorityQueue<AStarNode>(new NodeComparer());
@@ -72,52 +70,6 @@ public class PathFinder {
 		}
 		return new Path(locList);
 	}
-
-	/*private AStarNode search(AStarNode startNode, AStarNode endNode) {
-		System.out.println("start node: " + startNode);
-		System.out.println("end node: " + endNode);
-		PriorityQueue<AStarNode> openSet = new PriorityQueue<AStarNode>(new NodeComparer());
-		ArrayList<AStarNode> closedSet = new ArrayList<AStarNode>();
-		int nodesExpanded = 0;
-		openSet.add(startNode);
-		while (!openSet.peek().equals(endNode)) {
-			AStarNode currentNode = openSet.poll();
-			closedSet.add(currentNode);
-
-			for (AStarNode successorNode : getSuccessors(currentNode, endNode, closedSet)) {
-				double estimatedG = currentNode.g + 1;
-				boolean inOpenSet = openSet.contains(successorNode);
-
-				if (inOpenSet && estimatedG >= successorNode.g) {
-					continue;
-				}
-				if (inOpenSet && estimatedG < successorNode.g) {
-					openSet.remove(successorNode);
-					inOpenSet = false;
-				}
-				if (!inOpenSet){
-					successorNode.setParent(currentNode);				
-					successorNode.h = Math.max(Math.abs(successorNode.mapLoc.getX() - endNode.mapLoc.getX()),
-							Math.abs(successorNode.mapLoc.getY() - endNode.mapLoc.getY()));
-					successorNode.f = successorNode.h + successorNode.g;
-					System.out.println("f: "+ successorNode.f);
-					openSet.add(successorNode);
-					
-				}
-			}
-			System.out.println("open set: " + openSet);
-			System.out.println("closed set: " + closedSet);
-			System.out.println("nodes expanded: " + nodesExpanded);
-			nodesExpanded++;
-		}
-		for(AStarNode a : openSet) {
-			a.clearValues();
-		}
-		for(AStarNode a : closedSet) {
-			a.clearValues();
-		}
-		return endNode;
-	}*/
 
 	private ArrayList<AStarNode> getSuccessors(AStarNode currentNode, AStarNode endNode, ArrayList<AStarNode> closedSet) {
 		ArrayList<AStarNode> successors = new ArrayList<AStarNode>();
