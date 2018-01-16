5485ddc4f6a6bbcdc82a130ae4255468b7935357
KerbalsRock <bbishop19@windwardschool.org>
2018-01-16 11:02:26 -0800

    Austin coming in clutch

--- BasicUnitManager.java
+++ BasicUnitManager.java
@@ -3,11 +3,13 @@ import bc.GameController;
 
 public abstract class BasicUnitManager extends GameObjectManager {
 	
-	ArrayList<GameObject> units;
+	ArrayList<BasicUnit> units;
 	 
 	public BasicUnitManager(GameController gc, ArrayList<GameObject> unitList) {
 		super(gc, unitList);
-		units = unitList;
+		for(GameObject o : unitList) {
+			units.add((BasicUnit)o);
+		}
 	}
 
 }
